3e4640f016121c2607589fae78181b3c67c4fb02
= <a.wlezien@gmail.com>
2017-07-29 15:34:46 -0500

    fixed header bar and text

--- target/heroku/app/target/classes/static/scripts/jsonCtrl.js
+++ target/heroku/app/target/classes/static/scripts/jsonCtrl.js
@@ -33,13 +33,14 @@
     		            }
     		           
     				
+    				/*
     				$http.get("http://localhost:8000/api/json",config).then(function(response){
     					
     					console.log("yay get worked");
     					
     				})
     				
-    				/*
+    			
     				$http.post("http://localhost:8000/api/json",$scope.json,config).then(function (data, status, headers, config) {
     					
     					console.log("data: " + data.data.json);
